d2f88ba88aefc12eb20670193faded168f67ea52
Tim <tim@brightsidedeveloper.com>
2022-08-28 15:35:53 -0500

    Tailwind

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-
+import tw from 'twin.macro'
 
 function App() {
   return (
-    <></>
+    <>
+      <Text>Hello, World!</Text>
+      <Text>Awesome</Text>
+    </>
   );
 }
 
 export default App;
+
+const Text = tw.h1`bg-red-500	p-10 my-4 mx-auto rounded text-white w-1/3 text-center`;
